bc78a13073084f4d1558a2120a65b6662752916b
secret <dmitrii.rubanov@yandex.ru>
2021-04-04 10:22:12 +0300

    results

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, render_template
 
 app = Flask(__name__)
 
@@ -24,6 +24,14 @@ def promotion():
     <center><h1>И начнем с Марса!</h1><center>
     <div>
     <center><h1>Присоединяйся!</h1><center>"""
+
+
+@app.route('/index/<title>')
+def inde(title):
+    if title:
+        return render_template('base.html', title=title)
+    else:
+        return render_template('base.html', title='Заголовок')
 
 
 @app.route('/image_mars')
@@ -200,6 +208,34 @@ def astronaut_selection():
                         </html>'''
     elif request.method == 'POST':
         return "Форма отправлена"
+
+
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <title>Колонизация</title>
+                  </head>
+                  <body>
+<h1>Результаты отбора</h1>
+    <h3>Претендента на участие в миссии {nickname}:</h3>
+<div class="alert alert-success" role="alert">
+                      Поздравляем! Ваш рейтинг после {level} этапа отбора
+</div>
+<h3>Составляет {rating}!</h3>
+<div class="alert alert-warning" role="alert">
+                      Желаем удачи!
+</div>
+</body>
+                </html>"""
 
 
 if __name__ == '__main__':
